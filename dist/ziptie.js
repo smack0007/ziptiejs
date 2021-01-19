ae7581cd0f6e3bc7eed45b3af848d50c46a398ee
smack0007 <zachary.snow@gmail.com>
2021-01-19 13:08:37 +0100

    Reorganize project

--- dist/ziptie.js
+++ dist/ziptie.js
@@ -1,69 +1,73 @@
 "use strict";
-const attributeNameMap = (function () {
-    const typesToScan = [
-        EventTarget.prototype,
-        Node.prototype,
-        Element.prototype,
-        HTMLElement.prototype
-    ];
-    const map = {};
-    for (const type of typesToScan) {
-        for (const key of Object.keys(type)) {
-            map[key.toLowerCase()] = key;
+const ZipTie = (function () {
+    const attributeNameMap = (function () {
+        const typesToScan = [
+            EventTarget.prototype,
+            Node.prototype,
+            Element.prototype,
+            HTMLElement.prototype
+        ];
+        const map = {};
+        for (const type of typesToScan) {
+            for (const key of Object.keys(type)) {
+                map[key.toLowerCase()] = key;
+            }
         }
-    }
-    return map;
-})();
-function bind(view, model, parent) {
-    if (typeof view === "string") {
-        const selector = view;
-        view = document.querySelector(view);
-        if (view === null) {
-            throw new Error(`Failed to fetch a DOM element with the selector "${selector}".`);
-        }
-    }
-    console.info("bind", view);
-    let context = view["z-context"];
-    if (context === undefined) {
-        console.info("context not found...");
-        const viewBindings = {};
-        context = {
-            parent,
-            view,
-            viewBindings,
-            model,
-            getRoot: function () {
-                let root = this;
-                while (root.parent !== undefined) {
-                    root = root.parent;
-                }
-                return root;
-            },
-            updateView: function () {
-                console.info('updateView', this);
-                for (const viewKey of Object.keys(this.viewBindings)) {
-                    this.view[viewKey] = this.model[this.viewBindings[viewKey]];
-                }
-                for (const child of this.view.children) {
-                    bind(child, model, this);
+        return map;
+    })();
+    return {
+        bind: function (view, model, parent) {
+            if (typeof view === "string") {
+                const selector = view;
+                view = document.querySelector(view);
+                if (view === null) {
+                    throw new Error(`Failed to fetch a DOM element with the selector "${selector}".`);
                 }
             }
-        };
-        for (let i = 0; i < view.attributes.length; i++) {
-            const attribute = view.attributes[i];
-            if (attribute.name.startsWith(":")) {
-                viewBindings[attributeNameMap[attribute.name.substring(1)]] = attribute.value;
-            }
-            if (attribute.name.startsWith("@")) {
-                const eventName = "on" + attribute.name.substring(1);
-                const eventHandler = function (...args) {
-                    model[attribute.value](...args);
-                    context.getRoot().updateView();
+            console.info("bind", view);
+            let context = view["z-context"];
+            if (context === undefined) {
+                console.info("context not found...");
+                const viewBindings = {};
+                context = {
+                    parent,
+                    view,
+                    viewBindings,
+                    model,
+                    getRoot: function () {
+                        let root = this;
+                        while (root.parent !== undefined) {
+                            root = root.parent;
+                        }
+                        return root;
+                    },
+                    updateView: function () {
+                        console.info('updateView', this);
+                        for (const viewKey of Object.keys(this.viewBindings)) {
+                            this.view[viewKey] = this.model[this.viewBindings[viewKey]];
+                        }
+                        for (const child of this.view.children) {
+                            ZipTie.bind(child, model, this);
+                        }
+                    }
                 };
-                view[attributeNameMap[eventName]] = eventHandler;
+                for (let i = 0; i < view.attributes.length; i++) {
+                    const attribute = view.attributes[i];
+                    if (attribute.name.startsWith(":")) {
+                        viewBindings[attributeNameMap[attribute.name.substring(1)]] = attribute.value;
+                    }
+                    if (attribute.name.startsWith("@")) {
+                        const eventName = "on" + attribute.name.substring(1);
+                        const eventHandler = function (...args) {
+                            model[attribute.value](...args);
+                            context.getRoot().updateView();
+                        };
+                        view[attributeNameMap[eventName]] = eventHandler;
+                    }
+                }
+                view["z-context"] = context;
             }
+            context.updateView();
         }
-        view["z-context"] = context;
-    }
-    context.updateView();
-}
+    };
+})();
--- src/ziptie.ts
+++ src/ziptie.ts
@@ -1,91 +1,111 @@
 type AttributeNameMap = { [key: string]: string };
 
-const attributeNameMap = (function(): AttributeNameMap {
-    const typesToScan = [
-        EventTarget.prototype,
-        Node.prototype,
-        Element.prototype,
-        HTMLElement.prototype
-    ];
-    
-    const map: { [key: string]: string } = {};
+type ViewBindingsMap = { [key: string]: string };
 
-    for (const type of typesToScan) {
-        for (const key of Object.keys(type)) {
-            map[key.toLowerCase()] = key;
-        }
-    }
+interface BindContext {
+    parent?: BindContext;
 
-    return map;
-})();
+    view: HTMLElement;
 
-function bind(view: HTMLElement | string, model: any, parent?: any) {
-    if (typeof view === "string") {
-        const selector = view as string;
-        view = document.querySelector(view) as HTMLElement;
+    viewBindings: ViewBindingsMap;
 
-        if (view === null) {
-            throw new Error(`Failed to fetch a DOM element with the selector "${selector}".`);
-        }
-    }
+    model: any;
 
-    console.info("bind", view);
+    getRoot(): BindContext;
 
-    let context = (view as any)["z-context"];
-
-    if (context === undefined) {
-        console.info("context not found...");
-
-        const viewBindings: { [key: string]: string } = {};
-
-        context = {
-            parent,
-            view,
-            viewBindings,
-            model,
-            getRoot: function() {  
-                let root = this;
-
-                while (root.parent !== undefined) {
-                    root = root.parent;
-                }
-
-                return root;
-            },
-            updateView: function() {
-                console.info('updateView', this);
-
-                for (const viewKey of Object.keys(this.viewBindings)) {
-                    this.view[viewKey] = this.model[this.viewBindings[viewKey]];
-                }
-
-                for (const child of this.view.children) {
-                    bind(child, model, this);
-                }
-            }
-        };
-
-        for (let i = 0; i < view.attributes.length; i++) {
-            const attribute = view.attributes[i];
-
-            if (attribute.name.startsWith(":")) {
-                viewBindings[attributeNameMap[attribute.name.substring(1)]] = attribute.value;
-            }
-
-            if (attribute.name.startsWith("@")) {
-                const eventName = "on" + attribute.name.substring(1);
-                
-                const eventHandler = function(...args: any[]) {
-                    model[attribute.value](...args);
-                    context.getRoot().updateView();
-                };
-                
-                (view as any)[attributeNameMap[eventName]] = eventHandler;
-            }
-        }
-
-        (view as any)["z-context"] = context;
-    }
-
-    context.updateView();
+    updateView(): void;
 }
+
+const ZipTie = (function(){
+    const attributeNameMap = (function(): AttributeNameMap {
+        const typesToScan = [
+            EventTarget.prototype,
+            Node.prototype,
+            Element.prototype,
+            HTMLElement.prototype
+        ];
+        
+        const map: AttributeNameMap = {};
+
+        for (const type of typesToScan) {
+            for (const key of Object.keys(type)) {
+                map[key.toLowerCase()] = key;
+            }
+        }
+
+        return map;
+    })();
+
+    return {
+        bind: function(view: HTMLElement | string, model: any, parent?: any): void {
+            if (typeof view === "string") {
+                const selector = view as string;
+                view = document.querySelector(view) as HTMLElement;
+
+                if (view === null) {
+                    throw new Error(`Failed to fetch a DOM element with the selector "${selector}".`);
+                }
+            }
+
+            console.info("bind", view);
+
+            let context = (view as any)["z-context"] as BindContext;
+
+            if (context === undefined) {
+                console.info("context not found...");
+
+                const viewBindings: ViewBindingsMap = {};
+
+                context = {
+                    parent,
+                    view,
+                    viewBindings,
+                    model,
+                    getRoot: function() {  
+                        let root = this;
+
+                        while (root.parent !== undefined) {
+                            root = root.parent;
+                        }
+
+                        return root;
+                    },
+                    updateView: function() {
+                        console.info('updateView', this);
+
+                        for (const viewKey of Object.keys(this.viewBindings)) {
+                            (this.view as any)[viewKey] = this.model[this.viewBindings[viewKey]];
+                        }
+
+                        for (const child of this.view.children) {
+                            ZipTie.bind(child as HTMLElement, model, this);
+                        }
+                    }
+                } as BindContext;
+
+                for (let i = 0; i < view.attributes.length; i++) {
+                    const attribute = view.attributes[i];
+
+                    if (attribute.name.startsWith(":")) {
+                        viewBindings[attributeNameMap[attribute.name.substring(1)]] = attribute.value;
+                    }
+
+                    if (attribute.name.startsWith("@")) {
+                        const eventName = "on" + attribute.name.substring(1);
+                        
+                        const eventHandler = function(...args: any[]) {
+                            model[attribute.value](...args);
+                            context.getRoot().updateView();
+                        };
+                        
+                        (view as any)[attributeNameMap[eventName]] = eventHandler;
+                    }
+                }
+
+                (view as any)["z-context"] = context;
+            }
+
+            context.updateView();
+        }
+    }
+})();
